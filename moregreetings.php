90639c64fe656e216524ae54e73b966227cf0e77
Andrzej Rybitwa <andrzejrybitwa@gmail.com>
2026-09-11 11:32:31 +0200

    Poprawka - Sprawdzanie czy jest już taki contact_id w tabeli i ewentualnie ignoruje ponowne dodanie tego id

--- moregreetings.php
+++ moregreetings.php
@@ -84,7 +84,7 @@ function moregreetings_civicrm_post(string $op, string $objectName, int $objectI
   if ($op === 'edit' || $op === 'create') {
     if ($objectName === 'Individual' || $objectName === 'Organization' || $objectName === 'Household') {
       CRM_Core_DAO::executeQuery(
-        "INSERT IGNORE INTO civicrm_declinator_queue (contact_id) VALUES (%1)",
+        "INSERT INTO civicrm_declinator_queue (contact_id) VALUES (%1) ON DUPLICATE KEY UPDATE contact_id = %1;",
         [1 => [$objectId, 'Integer']]
       );
     }
